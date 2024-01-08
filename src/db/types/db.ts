fcb0bec51b9272cc0a9977a8cec10a5843c7bb66
Ivan Kashuba <ikashuba@meninx.net>
2024-01-08 22:18:57 +0200

    Change errors structure

--- src/db/types/db.ts
+++ src/db/types/db.ts
@@ -1,6 +1,6 @@
 import { Video } from '../../features/videos/types/model/Video';
 import { BlogViewModel } from '../../features/blogs/types/model/BlogModels';
-import { PostDbModel, PostViewModel } from '../../features/posts/types/model/PostModels';
+import { PostDbModel } from '../../features/posts/types/model/PostModels';
 
 export interface DataBase {
   videos: Video[];
